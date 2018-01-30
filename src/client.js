781afb8ebd625579db08398eaf5ea61ade67ae7f
Adam Rackis <adamrackis@gmail.com>
2018-01-30 17:04:36 -0600

    start caching

--- src/client.js
+++ src/client.js
@@ -5,14 +5,17 @@ export default class Client {
   constructor(props) {
     Object.assign(this, props);
     this.__mutationClient = new GraphQLClient(this.endpoint, this.fetchOptions);
+    if (this.cacheSize == null) {
+      this.cacheSize = 10;
+    }
   }
   runQuery(query, variables) {
-    return fetch(
-      `${this.endpoint}?query=${encodeURIComponent(compress(query))}${
-        typeof variables === "object" ? `&variables=${JSON.stringify(variables)}` : ""
-      }`,
-      this.fetchOptions || void 0
-    ).then(resp => resp.json());
+    return fetch(this.getGraphqlQuery({ query, variables }), this.fetchOptions || void 0).then(resp => resp.json());
+  }
+  getGraphqlQuery({ query, variables }) {
+    return `${this.endpoint}?query=${encodeURIComponent(compress(query))}${
+      typeof variables === "object" ? `&variables=${JSON.stringify(variables)}` : ""
+    }`;
   }
   runMutation(mutation, variables) {
     return this.__mutationClient.request(mutation, variables);
