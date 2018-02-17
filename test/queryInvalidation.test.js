4c79284bfb31f25539fd98b2c4a44269e2a19e2d
Adam Rackis <adamrackis@gmail.com>
2018-02-17 14:28:51 -0600

    more tests

--- test/queryInvalidation.test.js
+++ test/queryInvalidation.test.js
@@ -9,12 +9,14 @@ import { basicQuery, basicQueryWithVariables } from "./graphqlConstants";
 
 const client1 = new ClientMock("endpoint1");
 const client2 = new ClientMock("endpoint2");
+const client3 = new ClientMock("endpoint3");
 
 setDefaultClient(client1);
 
 beforeEach(() => {
   client1.reset();
   client2.reset();
+  client3.reset();
 });
 
 const DEFAULT_CACHE_SIZE = 10;
@@ -83,4 +85,56 @@ test("Override cache size", async () => {
   //call 1, cache miss
   Array.from({ length: 2 }).forEach((x, i) => obj.setProps({ page: 3 - i - 1 }));
   expect(client1.queriesRun).toBe(4);
+});
+
+//TODO: remove when deprecation is gone
+test("Default cache size with deprecated client", async () => {
+  let Component = getComponent(client3, basicQueryWithVariablesPacket);
+  let obj = mount(<Component page={1} unused={10} />);
+
+  Array.from({ length: 9 }).forEach((x, i) => obj.setProps({ page: i + 2 }));
+  expect(client3.queriesRun).toBe(10);
+
+  Array.from({ length: 9 }).forEach((x, i) => obj.setProps({ page: 10 - i - 1 }));
+  expect(client3.queriesRun).toBe(10);
+});
+
+//TODO: remove when deprecation is gone
+test("Override cache size with deprecated client", async () => {
+  let Component = getComponent(client3, basicQueryWithVariablesPacket, { cacheSize: 2 });
+  let obj = mount(<Component page={1} unused={10} />);
+
+  //3 is a cache ejection, cache is now 2,3
+  Array.from({ length: 2 }).forEach((x, i) => obj.setProps({ page: i + 2 }));
+  expect(client3.queriesRun).toBe(3);
+
+  //call 2, cache hit, cache is now 2,3
+  //call 1, cache miss
+  Array.from({ length: 2 }).forEach((x, i) => obj.setProps({ page: 3 - i - 1 }));
+  expect(client3.queriesRun).toBe(4);
+});
+
+test("Default cache size with overridden client", async () => {
+  let Component = getComponent(basicQueryWithVariablesPacket, { client: client2 });
+  let obj = mount(<Component page={1} unused={10} />);
+
+  Array.from({ length: 9 }).forEach((x, i) => obj.setProps({ page: i + 2 }));
+  expect(client2.queriesRun).toBe(10);
+
+  Array.from({ length: 9 }).forEach((x, i) => obj.setProps({ page: 10 - i - 1 }));
+  expect(client2.queriesRun).toBe(10);
+});
+
+test("Override cache size with overridden client", async () => {
+  let Component = getComponent(basicQueryWithVariablesPacket, { cacheSize: 2, client: client2 });
+  let obj = mount(<Component page={1} unused={10} />);
+
+  //3 is a cache ejection, cache is now 2,3
+  Array.from({ length: 2 }).forEach((x, i) => obj.setProps({ page: i + 2 }));
+  expect(client2.queriesRun).toBe(3);
+
+  //call 2, cache hit, cache is now 2,3
+  //call 1, cache miss
+  Array.from({ length: 2 }).forEach((x, i) => obj.setProps({ page: 3 - i - 1 }));
+  expect(client2.queriesRun).toBe(4);
 });
