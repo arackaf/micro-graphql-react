acfd7b6d6d9e06fccf18f050ad46ff387f9f8443
Adam Rackis <adamrackis@hotmail.com>
2018-03-04 16:25:45 -0600

    tests and docs on new compress method

---
+++ test/compress.test.js
@@ -0,0 +1,19 @@
+import compress from "../src/compress";
+
+const title = `This    is    a      not   compressed`;
+
+const query1 = compress`
+  query ReadBooks () {
+     allBooks (title: "${title}") {
+       Books {
+         title
+         publisher
+       }
+     }
+  `;
+
+const compressed1 = `query ReadBooks(){allBooks(title: "${title}"){Books{title publisher}}`;
+
+test("Compress 1", async () => {
+  expect(compressed1).toEqual(query1);
+});
