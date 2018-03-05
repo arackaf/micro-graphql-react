eb5b5b91caced2dea5bb4bee173c5ee95c697db9
Adam Rackis <adamrackis@gmail.com>
2018-03-05 11:18:29 -0600

    compress tweaks, with a new test

--- test/compress.test.js
+++ test/compress.test.js
@@ -15,7 +15,7 @@ const query1 = compress`
 const compressed1 = `query ReadBooks{allBooks(title:"${title}"){Books{title publisher}}}`;
 
 test("Compress 1", async () => {
-  expect(compressed1).toEqual(query1);
+  expect(query1).toEqual(compressed1);
 });
 
 const query2 = compress`
@@ -31,7 +31,7 @@ const query2 = compress`
 const compressed2 = `allBooks(title:"${title}"){Books{title publisher}}`;
 
 test("Compress 2", async () => {
-  expect(compressed2).toEqual(query2);
+  expect(query2).toEqual(compressed2);
 });
 
 const query3 = compress`
@@ -53,5 +53,27 @@ const query3 = compress`
 const compressed3 = `query twoQueries{ab1:allBooks(title:"A"){Books{title publisher}}ab2:allBooks(title:"B"){Books{title publisher}}}`;
 
 test("Compress 3", async () => {
-  expect(compressed3).toEqual(query3);
+  expect(query3).toEqual(compressed3);
+});
+
+const query4 = compress`
+  query twoQueries ( $title1:  String , $title2 : String ) {
+    ab1: allBooks ( title : $title1 ) {
+      Books {
+         title
+         publisher
+      }
+    }
+    ab2: allBooks ( title : $title2 ) {
+      Books {
+         title
+         publisher
+      }
+    }
+  }`;
+
+const compressed4 = `query twoQueries($title1:String,$title2:String){ab1:allBooks(title:$title1){Books{title publisher}}ab2:allBooks(title:$title2){Books{title publisher}}}`;
+
+test("Compress 4", async () => {
+  expect(query4).toEqual(compressed4);
 });
