302ab6f8d758140cf39afa98b8a245012b1fd8a3
Adam Rackis <adamrackis@hotmail.com>
2018-05-31 12:05:41 -0500

    improve client mock for use with cache testing

--- test/clientMock.js
+++ test/clientMock.js
@@ -1,7 +1,9 @@
 import compress from "graphql-query-compress";
+import ClientBase from "../src/client";
 
-export default class Client {
+export default class Client extends ClientBase {
   constructor(endpoint) {
+    super(endpoint);
     this.endpoint = endpoint;
     this.reset();
   }
@@ -12,17 +14,11 @@ export default class Client {
     this.mutationsRun = 0;
     this.mutationCalls = [];
   };
-
   runQuery = (query, variables) => {
     this.queriesRun++;
     this.queryCalls.push([query, variables]);
     return this.nextResult || {};
   };
-  getGraphqlQuery({ query, variables }) {
-    return `${this.endpoint}?query=${encodeURIComponent(compress(query))}${
-      typeof variables === "object" ? `&variables=${JSON.stringify(variables)}` : ""
-    }`;
-  }
   runMutation = (mutation, variables) => {
     this.mutationsRun++;
     this.mutationCalls.push([mutation, variables]);
