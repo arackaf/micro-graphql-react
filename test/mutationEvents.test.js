4cde23e1d80e2e106f77c71a158017fe79068a30
Adam Rackis <adamrackis@gmail.com>
2018-06-02 19:43:10 -0500

    Start to add mutation events

---
+++ test/mutationEvents.test.js
@@ -0,0 +1,46 @@
+import {
+  React,
+  Component,
+  shallow,
+  mount,
+  ClientMock,
+  query,
+  mutation,
+  setDefaultClient,
+  basicQuery,
+  basicQueryWithVariables
+} from "./testSuiteInitialize";
+
+let client1;
+let client2;
+let client3;
+
+beforeEach(() => {
+  client1 = new ClientMock("endpoint1");
+  client2 = new ClientMock("endpoint2");
+  client3 = new ClientMock("endpoint3");
+  setDefaultClient(client1);
+});
+
+const DEFAULT_CACHE_SIZE = 10;
+
+const getMutationComponent = (...args) =>
+  @mutation(`mutation{}`)
+  @query(...args)
+  class extends Component {
+    render = () => null;
+  };
+
+const basicMutationPacket = [basicQueryWithVariables, props => ({ page: props.page })];
+
+test("Default cache size", async () => {
+  let Component = getMutationComponent(...basicMutationPacket);
+  let obj = shallow(<Component page={1} unused={10} />).dive();
+
+  console.log("instance", obj.props());
+  Array.from({ length: 9 }).forEach((x, i) => obj.setProps({ page: i + 2 }));
+  expect(client1.queriesRun).toBe(10);
+
+  Array.from({ length: 9 }).forEach((x, i) => obj.setProps({ page: 10 - i - 1 }));
+  expect(client1.queriesRun).toBe(10);
+});
--- test/testSuiteInitialize.js
+++ test/testSuiteInitialize.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Enzyme, { mount } from "enzyme";
+import Enzyme, { mount, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -7,4 +7,4 @@ import ClientMock from "./clientMock";
 import { query, mutation, setDefaultClient } from "../index-local";
 import { basicQuery, basicQueryWithVariables } from "./graphqlConstants";
 
-export { React, Component, mount, ClientMock, query, mutation, setDefaultClient, basicQuery, basicQueryWithVariables };
+export { React, Component, mount, shallow, ClientMock, query, mutation, setDefaultClient, basicQuery, basicQueryWithVariables };
