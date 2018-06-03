e31f7faf3e221c4fba0f079b93a2178b6326ed2e
Adam Rackis <adamrackis@gmail.com>
2018-06-03 13:52:43 -0500

    test updates

--- test/mutationEvents.test.js
+++ test/mutationEvents.test.js
@@ -13,7 +13,7 @@ beforeEach(() => {
 
 const DEFAULT_CACHE_SIZE = 10;
 
-const getMutationComponent = queryArgs =>
+const getQueryAndMutationComponent = queryArgs =>
   @mutation(`someMutation{}`)
   @query(...queryArgs)
   class extends Component {
@@ -25,7 +25,7 @@ const queryPacket2 = [basicQueryWithVariables, props => ({ query: props.query })
 
 test("Mutation listener runs with exact match", async () => {
   let runCount = 0;
-  let Component = getMutationComponent(queryPacket.concat({ onMutation: { when: "updateBook", run: () => runCount++ } }));
+  let Component = getQueryAndMutationComponent(queryPacket.concat({ onMutation: { when: "updateBook", run: () => runCount++ } }));
   let obj = shallow(<Component page={1} />).dive();
 
   client1.nextMutationResult = { updateBook: { Book: { title: "New Title" } } };
@@ -36,7 +36,7 @@ test("Mutation listener runs with exact match", async () => {
 
 test("Mutation listener runs with exact match", async () => {
   let runCount = 0;
-  let Component = getMutationComponent(queryPacket.concat({ onMutation: { when: "updateBook", run: () => runCount++ } }));
+  let Component = getQueryAndMutationComponent(queryPacket.concat({ onMutation: { when: "updateBook", run: () => runCount++ } }));
   let obj = shallow(<Component page={1} />).dive();
 
   client1.nextMutationResult = { updateAuthor: { Author: { name: "New Name" } } };
@@ -46,7 +46,7 @@ test("Mutation listener runs with exact match", async () => {
 });
 
 test("Mutation listener updates cache", async () => {
-  let Component = getMutationComponent(
+  let Component = getQueryAndMutationComponent(
     queryPacket2.concat({
       onMutation: {
         when: "updateBook",
@@ -60,21 +60,19 @@ test("Mutation listener updates cache", async () => {
     })
   );
 
-  client1.nextResult = { data: { Books: [{ id: 1, title: "Book 1", author: "Adam" }, { id: 2, title: "Book 2", author: "Evee" }] } };
+  client1.nextResult = { data: { Books: [{ id: 1, title: "Book 1", author: "Adam" }, { id: 2, title: "Book 2", author: "__WRONG__Eve" }] } };
   let obj = shallow(<Component query="a" />).dive();
   await waitAndUpdate(obj);
 
   client1.nextResult = { data: { Books: [{ id: 1, title: "Book 1", author: "Adam" }] } };
-  obj.setProps({ query: "b" });
-  await waitAndUpdate(obj);
+  await setPropsAndWait(obj, { query: "b" });
 
   client1.nextMutationResult = { updateBook: { Book: { id: 2, author: "Eve" } } };
   await obj.props().runMutation();
 
   expect(client1.queriesRun).toBe(2); //run for new query args
 
-  obj.setProps({ query: "a" });
-  await waitAndUpdate(obj);
+  await setPropsAndWait(obj, { query: "a" });
 
   expect(client1.queriesRun).toBe(2); //still loads from cache
   expect(obj.props().data).toEqual({ Books: [{ id: 1, title: "Book 1", author: "Adam" }, { id: 2, title: "Book 2", author: "Eve" }] }); //loads updated data
@@ -83,6 +81,10 @@ test("Mutation listener updates cache", async () => {
 //console.log("current query", obj.instance().currentGraphqlQuery);
 //console.log("instance", obj.props());
 
+async function setPropsAndWait(renderedComponent, props) {
+  renderedComponent.setProps(props);
+  await waitAndUpdate(renderedComponent);
+}
 function waitAndUpdate(renderedComponent) {
   return new Promise(res => {
     setTimeout(() => {
