0913f2951b11e78e15a3c3e17586f8830381239d
Adam Rackis <arackis@centralreach.com>
2018-07-05 15:16:29 -0500

    More query work

--- src/query.js
+++ src/query.js
@@ -1,97 +1,13 @@
 import React, { Component } from "react";
 import { defaultClientManager } from "./client";
-
-const setPendingResultSymbol = Symbol("setPendingResult");
-const setResultsSymbol = Symbol("setResults");
-const getFromCacheSymbol = Symbol("getFromCache");
-const noCachingSymbol = Symbol("noCaching");
-const cacheSymbol = Symbol("cache");
-class QueryCache {
-  constructor(cacheSize = 0) {
-    this.cacheSize = cacheSize;
-  }
-  [cacheSymbol] = new Map([]);
-  get [noCachingSymbol]() {
-    return !this.cacheSize;
-  }
-
-  get entries() {
-    return [...this[cacheSymbol]];
-  }
-
-  get(key) {
-    return this[cacheSymbol].get(key);
-  }
-
-  set(key, results) {
-    this[cacheSymbol].set(key, results);
-  }
-
-  delete(key) {
-    this[cacheSymbol].delete(key);
-  }
-
-  clearCache() {
-    this[cacheSymbol].clear();
-  }
-
-  [setPendingResultSymbol](graphqlQuery, promise) {
-    let cache = this[cacheSymbol];
-    //front of the line now, to support LRU ejection
-    if (!this[noCachingSymbol]) {
-      cache.delete(graphqlQuery);
-      if (cache.size === this.cacheSize) {
-        //maps iterate entries and keys in insertion order - zero'th key should be oldest
-        cache.delete([...cache.keys()][0]);
-      }
-      cache.set(graphqlQuery, promise);
-    }
-  }
-
-  [setResultsSymbol](promise, cacheKey, resp, err) {
-    let cache = this[cacheSymbol];
-    if (this[noCachingSymbol]) {
-      return;
-    }
-
-    //cache may have been cleared while we were running. If so, we'll respect that, and not touch the cache, but
-    //we'll still use the results locally
-    if (cache.get(cacheKey) !== promise) return;
-
-    if (err) {
-      cache.set(cacheKey, { data: null, error: err });
-    } else {
-      if (resp.errors) {
-        cache.set(cacheKey, { data: null, error: errors });
-      } else {
-        cache.set(cacheKey, { data: resp.data, error: null });
-      }
-    }
-  }
-
-  [getFromCacheSymbol](key, ifPending, ifResults, ifNotFound) {
-    let cache = this[cacheSymbol];
-    if (this[noCachingSymbol]) {
-      ifNotFound();
-    } else {
-      let cachedEntry = cache.get(key);
-      if (cachedEntry) {
-        if (typeof cachedEntry.then === "function") {
-          ifPending(cachedEntry);
-        } else {
-          //re-insert to put it at the fornt of the line
-          cache.delete(key);
-          this.set(key, cachedEntry);
-          ifResults(cachedEntry);
-        }
-      } else {
-        ifNotFound();
-      }
-    }
-  }
-}
-
-const DEFAULT_CACHE_SIZE = 10;
+import QueryCache, {
+  setPendingResultSymbol,
+  setResultsSymbol,
+  getFromCacheSymbol,
+  noCachingSymbol,
+  cacheSymbol,
+  DEFAULT_CACHE_SIZE
+} from "./queryCache";
 
 export default (query, variablesFn, packet = {}) => BaseComponent => {
   if (typeof variablesFn === "object") {
