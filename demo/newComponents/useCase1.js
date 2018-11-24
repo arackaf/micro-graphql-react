48c61b1f6b831a6e7a276de86d5893b898e62d30
Adam Rackis <adamrackis@gmail.com>
2018-11-24 13:13:24 -0600

    More docs / demo work

---
+++ demo/newComponents/useCase1.js
@@ -0,0 +1,77 @@
+import React, { Component, Fragment } from "react";
+import { GraphQL, buildQuery, buildMutation } from "../../index-local";
+import { BOOKS_QUERY, MODIFY_BOOK_TITLE, MODIFY_BOOK_PAGE } from "../savedQueries";
+
+export class BookQueryComponent1 extends Component {
+  render() {
+    return (
+      <div>
+        <GraphQL query={{ books: buildQuery(BOOKS_QUERY, { page: this.props.page }) }}>
+          {({ books: { data } }) =>
+            data ? (
+              <ul>
+                {data.allBooks.Books.map(b => (
+                  <li key={b._id}>
+                    {b.title} - {b.pages}
+                  </li>
+                ))}
+              </ul>
+            ) : null
+          }
+        </GraphQL>
+      </div>
+    );
+  }
+}
+
+//-------------------------------------------------------------------------------------------------
+
+export class BookEditing extends Component {
+  state = { editingId: "", editingOriginaltitle: "" };
+  edit = book => {
+    this.setState({ editingId: book._id, editingOriginaltitle: book.title, editingOriginalpages: book.pages });
+  };
+  render() {
+    let { editingId, editingOriginaltitle, editingOriginalpages } = this.state;
+    return (
+      <GraphQL
+        query={{ books: buildQuery(BOOKS_QUERY, { page: this.props.page }) }}
+        mutation={{
+          titleMutation: buildMutation(MODIFY_BOOK_TITLE),
+          pagesMutation: buildMutation(MODIFY_BOOK_PAGE)
+        }}
+      >
+        {({ books: { loading, loaded, data }, titleMutation, pagesMutation }) => (
+          <div>
+            {loading ? <div>LOADING</div> : null}
+            {loaded ? <div>LOADED</div> : null}
+            {data ? (
+              <ul>
+                {data.allBooks.Books.map(book => (
+                  <li key={book._id}>
+                    {book.title}
+                    <button onClick={() => this.edit(book)}> edit</button>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+
+            {editingId ? (
+              <Fragment>
+                {titleMutation.running ? <div>RUNNING</div> : null}
+                {titleMutation.finished ? <div>SAVED</div> : null}
+                <input defaultValue={editingOriginaltitle} ref={el => (this.el = el)} placeholder="New title here!" />
+                <button onClick={() => titleMutation.runMutation({ _id: editingId, title: this.el.value })}>Save</button>
+
+                {pagesMutation.running ? <div>RUNNING</div> : null}
+                {pagesMutation.finished ? <div>SAVED</div> : null}
+                <input defaultValue={editingOriginalpages} ref={el => (this.elPages = el)} placeholder="New pages here!" />
+                <button onClick={() => pagesMutation.runMutation({ _id: editingId, pages: +this.elPages.value })}>Save</button>
+              </Fragment>
+            ) : null}
+          </div>
+        )}
+      </GraphQL>
+    );
+  }
+}
