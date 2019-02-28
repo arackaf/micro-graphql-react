fd97631d541a57b24dcdda81eff831c47559c647
Adam Rackis <adamrackis@gmail.com>
2019-02-28 11:20:04 -0600

    build cleanup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Client, { defaultClientManager } from "./client";
+import compress from "./compress";
+import GraphQL from "./gqlComponent";
+import Cache from "./cache";
+import useQuery from "./useQuery";
+import useMutation from "./useMutation";
+import { buildQuery, buildMutation } from "./util";
+
+const { setDefaultClient, getDefaultClient } = defaultClientManager;
+
+export { Client, compress, setDefaultClient, getDefaultClient, GraphQL, buildQuery, buildMutation, Cache, useQuery, useMutation };
