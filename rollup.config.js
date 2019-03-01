f55e140c7c2c5a238535a5f35b9666fe5a5e4dc1
Adam Rackis <adamrackis@gmail.com>
2019-02-28 21:03:28 -0600

    Jest working again

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ const resolve = require("rollup-plugin-node-resolve");
 const { terser } = require("rollup-plugin-terser");
 const path = require("path");
 
-const getConfig = ({ file, minify = true, presets = [], plugins = [] }) => ({
+const getConfig = ({ file, minify = false, presets = [], plugins = [] }) => ({
   input: "./src/index.js",
   output: {
     format: "esm",
@@ -13,11 +13,12 @@ const getConfig = ({ file, minify = true, presets = [], plugins = [] }) => ({
   external: ["react", "react-dom"],
   plugins: [
     babel({
+      babelrc: false,
       exclude: "node_modules/**",
       presets: ["@babel/preset-react", ...presets],
       plugins: ["@babel/plugin-proposal-class-properties", ...plugins]
     }),
-    //minify && terser({}),
+    minify && terser({}),
     resolve({}),
     commonjs({ include: ["node_modules/**"] })
   ]
@@ -27,7 +28,7 @@ let es5config = ["@babel/preset-env", { targets: { ie: "11" } }];
 
 module.exports = [
   getConfig({ file: "index.js" }),
-  //getConfig({ file: "index-debug.js", minify: false }),
+  getConfig({ file: "index.min.js", minify: true }),
   getConfig({ file: "index-es5.js", presets: [es5config], plugins: ["@babel/plugin-proposal-object-rest-spread"] }),
-  //getConfig({ file: "index-es5-debug.js", minify: false, presets: [es5config], plugins: ["@babel/plugin-proposal-object-rest-spread"] })
+  getConfig({ file: "index-es5.min.js", minify: true, presets: [es5config], plugins: ["@babel/plugin-proposal-object-rest-spread"] })
 ];
