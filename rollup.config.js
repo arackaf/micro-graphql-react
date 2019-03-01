d13978d6cf3f2947b6ce9d728389aeda27cf626c
Adam.Rackis <adam.rackis@riotgames.com>
2019-02-28 19:13:13 -0600

    build work

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ const resolve = require("rollup-plugin-node-resolve");
 const { terser } = require("rollup-plugin-terser");
 const path = require("path");
 
-const getConfig = ({ file, minify = true }) => ({
+const getConfig = ({ file, minify = true, presets = [], plugins = [] }) => ({
   input: "./src/index.js",
   output: {
     format: "esm",
@@ -14,13 +14,20 @@ const getConfig = ({ file, minify = true }) => ({
   plugins: [
     babel({
       exclude: "node_modules/**",
-      presets: ["@babel/preset-react"],
-      plugins: ["@babel/plugin-proposal-class-properties"]
+      presets: ["@babel/preset-react", ...presets],
+      plugins: ["@babel/plugin-proposal-class-properties", ...plugins]
     }),
-    minify && terser({}),
+    //minify && terser({}),
     resolve({}),
     commonjs({ include: ["node_modules/**"] })
   ]
 });
 
-module.exports = [getConfig({ file: "index.js" }), getConfig({ file: "index-debug.js", minify: false })];
+let es5config = ["@babel/preset-env", { targets: { ie: "11" } }];
+
+module.exports = [
+  getConfig({ file: "index.js" }),
+  //getConfig({ file: "index-debug.js", minify: false }),
+  getConfig({ file: "index-es5.js", presets: [es5config], plugins: ["@babel/plugin-proposal-object-rest-spread"] }),
+  //getConfig({ file: "index-es5-debug.js", minify: false, presets: [es5config], plugins: ["@babel/plugin-proposal-object-rest-spread"] })
+];
