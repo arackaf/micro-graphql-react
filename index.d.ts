550624deff6dad34cc365d101e3aca78db2d99cd
Adam.Rackis <adam.rackis@riotgames.com>
2019-11-09 17:46:25 -0600

    Temporarily export queryManager for suspense experimentation

--- index.d.ts
+++ index.d.ts
@@ -27,6 +27,8 @@ export type QueryPayload<TResults = any> = {
   clearCacheAndReload: () => void;
 };
 
+export const QueryManager: any;
+
 export type MutationPayload<TResults = any> = {
   running: boolean;
   finished: boolean;
