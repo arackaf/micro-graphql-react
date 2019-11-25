2adaa5cedd1fabb3ffab8b3984b970c0e27e1c0f
Adam.Rackis <adam.rackis@riotgames.com>
2019-11-25 01:05:33 -0500

    Add test for sync resolve on data that's present already

---
+++ test/hooks/cachedResultSynchronouslyAvailable.test.js
@@ -0,0 +1,33 @@
+import { React, render, ClientMock, setDefaultClient, useQuery, useMutation } from "../testSuiteInitialize";
+import { pause } from "../testUtils";
+
+const LOAD_TASKS = "A";
+const LOAD_USERS = "B";
+const UPDATE_USER = "M";
+
+let client1;
+let ComponentToUse;
+let renders = 0;
+
+beforeEach(() => {
+  client1 = new ClientMock("endpoint1");
+  setDefaultClient(client1);
+  renders = 0;
+});
+
+test("Cache object behaves correctly", async () => {
+  const ComponentToUse = props => {
+    const loadTasks = useQuery([LOAD_TASKS, { a: 12 }]);
+    return null;
+  };
+
+  const ComponentToUseNext = props => {
+    const loadTasks = useQuery([LOAD_TASKS, { a: 12 }]);
+    expect(loadTasks.data).not.toBeNull();
+    return null;
+  };
+
+  client1.nextResult = { data: {} };
+  let { rerender } = render(<ComponentToUse />);
+  let { rerender_next } = render(<ComponentToUseNext />);
+});
