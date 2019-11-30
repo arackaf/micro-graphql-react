3c9878996346749fd91e9dfef675da2e9d17247e
Adam Rackis <adamrackis@gmail.com>
2019-11-29 20:20:52 -0600

    subscriptions only run when active

--- src/queryManager.js
+++ src/queryManager.js
@@ -47,7 +47,8 @@ export default class QueryManager {
         softReset: this.softReset,
         hardReset: this.hardReset,
         refresh: this.refresh,
-        currentResults: () => this.currentState.data
+        currentResults: () => this.currentState.data,
+        isActive: () => this.active
       });
     }
   }
