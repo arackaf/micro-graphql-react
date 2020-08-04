1300b14e4b1e224f3cb2a60d7d9ae82fadde7b81
Adam Rackis <adamrackis@gmail.com>
2020-08-03 23:23:47 -0400

    Initial state synchronously available

--- src/queryManager.js
+++ src/queryManager.js
@@ -20,11 +20,26 @@ export default class QueryManager {
     this.suspense = suspense;
     this.preloadOnly = preloadOnly;
 
+    let existingState = {};
+    if (isActive) {
+      let graphqlQuery = this.client.getGraphqlQuery({ query, variables });
+
+      this.cache.getFromCache(
+        graphqlQuery,
+        promise => {},
+        cachedEntry => {
+          existingState = { data: cachedEntry.data, error: cachedEntry.error || null, loading: false, loaded: true, currentQuery: graphqlQuery };
+        },
+        () => {}
+      );
+    }
+
     Object.assign(this.currentState, {
       reload: this.reload,
       clearCache: () => this.cache.clearCache(),
       clearCacheAndReload: this.clearCacheAndReload,
-      isActive
+      isActive,
+      ...existingState
     });
   }
   init() {
