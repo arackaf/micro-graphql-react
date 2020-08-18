c71cb12ecdcebb7c0d40582f8eef0f8f4504fc82
Adam Rackis <adamrackis@gmail.com>
2020-08-17 22:35:10 -0500

    More cleanup

--- demo/server.js
+++ demo/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, response) => response.sendfile("./demo/dist/index.html"));
+app.get("/", (req, response) => response.sendFile(path.join(__dirname, "./dist/index.html")));
 app.use("/dist/", express.static(__dirname + "/dist/"));
 
 app.listen(process.env.PORT || 8080);
