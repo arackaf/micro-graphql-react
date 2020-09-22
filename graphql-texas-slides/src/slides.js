29ced05ef10a55238c1e53548b4cebff7cb1ea40
Adam Rackis <adamrackis@gmail.com>
2020-09-22 13:00:47 -0500

    Slide updates

--- graphql-texas-slides/src/slides.js
+++ graphql-texas-slides/src/slides.js
@@ -664,12 +664,52 @@ const Presentation = () => (
       </FlexBox>
     </Slide>
     <Slide transitionEffect="slide">
+      <Heading>About me</Heading>
+
+      <UnorderedList>
+        <ListItem>Software Engineer at Riot</ListItem>
+        <ListItem>
+          <FlexBox justifyContent="start">
+            <svg width="45" height="45" viewBox="0 0 24 24">
+              <g>
+                <path
+                  style={{ fill: "rgba(29,161,242,1.00)" }}
+                  d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"
+                ></path>
+              </g>
+            </svg>
+            <span>&nbsp;@adamrackis</span>
+          </FlexBox>
+        </ListItem>
+        <ListItem>
+          <FlexBox justifyContent="start">
+            <svg class="octicon octicon-mark-github v-align-middle" height="45" viewBox="0 0 16 16" version="1.1" width="45" aria-hidden="true">
+              <path
+                style={{ fill: "white" }}
+                fill-rule="evenodd"
+                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+              ></path>
+            </svg>
+            <span>&nbsp;arackaf</span>
+          </FlexBox>
+        </ListItem>
+      </UnorderedList>
+    </Slide>
+    <Slide transitionEffect="slide">
+      <Heading>These slides</Heading>
+
+      <UnorderedList>
+        <ListItem>Are in the micro-graphql-react repo</ListItem>
+        <ListItem>There's a link to the actual slideshow in the readme</ListItem>
+      </UnorderedList>
+    </Slide>
+    <Slide transitionEffect="slide">
       <Heading>A Few Things</Heading>
 
-      <OrderedList>
+      <UnorderedList>
         <ListItem>Every GraphQL client has tradeoffs</ListItem>
         <ListItem>This talk is about identifying tradeoffs, not picking a "better" client</ListItem>
-      </OrderedList>
+      </UnorderedList>
     </Slide>
 
     <Slide transitionEffect="slide">
@@ -718,7 +758,7 @@ const Presentation = () => (
     <Slide transitionEffect="slide">
       <Heading>From __typename to caching</Heading>
 
-      <OrderedList>
+      <UnorderedList>
         <ListItem>GraphQL clients usually add __typename to queries and mutations</ListItem>
         <ListItem>Keep track of queries run</ListItem>
         <ListItem>Keep track of mutations executed</ListItem>
@@ -727,7 +767,7 @@ const Presentation = () => (
         <Appear elementNum={0}>
           <ListItem>There's a Catch</ListItem>
         </Appear>
-      </OrderedList>
+      </UnorderedList>
     </Slide>
 
     <Slide transitionEffect="slide">
@@ -741,7 +781,7 @@ const Presentation = () => (
     </Slide>
     <Slide>
       <Heading>Approach #1: Normalized Cache</Heading>
-      <OrderedList>
+      <UnorderedList>
         <Appear elementNum={0}>
           <ListItem>Each item from each result set stored by id and type</ListItem>
         </Appear>
@@ -751,7 +791,7 @@ const Presentation = () => (
         <Appear elementNum={2}>
           <ListItem>After mutating an item, it's updated in cache, and all query result sets are immediately updated! 🥳 🎉 🍾</ListItem>
         </Appear>
-      </OrderedList>
+      </UnorderedList>
     </Slide>
     <Slide>
       <Heading>Example</Heading>
@@ -863,10 +903,10 @@ const Presentation = () => (
     <Slide transitionEffect="slide">
       <Heading>It Just Works</Heading>
 
-      <OrderedList>
+      <UnorderedList>
         <ListItem>Book #2 is updated in cache</ListItem>
         <ListItem>All queries with Book #2 in its results automatically see updated values</ListItem>
-      </OrderedList>
+      </UnorderedList>
     </Slide>
 
     <Slide>
@@ -929,7 +969,7 @@ const Presentation = () => (
     <Slide transitionEffect="slide">
       <Heading>What Should Have Happened</Heading>
 
-      <OrderedList>
+      <UnorderedList>
         <ListItem>
           Task #2 should have been <b>ejected</b> from that result set
         </ListItem>
@@ -939,13 +979,13 @@ const Presentation = () => (
         </ListItem>
         <ListItem>Please don't take my word for it - evaluate any tool you're considering using.</ListItem>
         <ListItem>This talk is about identifying tradeoffs - not judging which client is better.</ListItem>
-      </OrderedList>
+      </UnorderedList>
     </Slide>
 
     <Slide transitionEffect="slide">
       <Heading>Does this mean normalized caching is bad?</Heading>
 
-      <OrderedList>
+      <UnorderedList>
         <Appear elementNum={0}>
           <ListItem>Of course not</ListItem>
         </Appear>
@@ -955,12 +995,12 @@ const Presentation = () => (
         <Appear elementNum={2}>
           <ListItem>Just know what they are before you decide on a solution, and confirm they fit with your application's needs</ListItem>
         </Appear>
-      </OrderedList>
+      </UnorderedList>
     </Slide>
 
     <Slide>
       <Heading style={{ marginBottom: 0 }}>URQL</Heading>
-      <OrderedList style={{ marginTop: 0 }}>
+      <UnorderedList style={{ marginTop: 0 }}>
         <Appear elementNum={0}>
           <FlexBox alignItems="center">
             <Image
@@ -969,11 +1009,11 @@ const Presentation = () => (
             />
           </FlexBox>
         </Appear>
-      </OrderedList>
+      </UnorderedList>
     </Slide>
     <Slide>
       <Heading>Approach #2: Urql</Heading>
-      <OrderedList>
+      <UnorderedList>
         <Appear elementNum={0}>
           <ListItem>
             Keep track of which <span style={{ fontWeight: "bold" }}>types</span> each query returns
@@ -984,7 +1024,7 @@ const Presentation = () => (
             When any mutation modifies data of that type, invalidate the <span style={{ fontWeight: "bold" }}>entire result set</span>
           </ListItem>
         </Appear>
-      </OrderedList>
+      </UnorderedList>
     </Slide>
     <Slide>
       <Heading>Urql in practice</Heading>
@@ -1036,7 +1076,7 @@ const Presentation = () => (
     </Slide>
     <Slide>
       <Heading>This is perfect right?</Heading>
-      <OrderedList>
+      <UnorderedList>
         <Appear elementNum={0}>
           <ListItem>
             Keep track of which <span style={{ fontWeight: "bold" }}>types</span> each query returns
@@ -1047,7 +1087,7 @@ const Presentation = () => (
             When any mutation modifies data of that type, invalidate the <span style={{ fontWeight: "bold" }}>entire result set</span>
           </ListItem>
         </Appear>
-      </OrderedList>
+      </UnorderedList>
     </Slide>
 
     <Slide>
