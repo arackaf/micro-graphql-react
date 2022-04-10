525234f5b5a63ed458bff785f209afde4f6db51c
Adam Rackis <adamrackis@hotmail.com>
2022-04-09 20:59:26 -0500

    Manually copy Vite-built graphql slides

--- graphql-texas-slides/vite.config.js
+++ graphql-texas-slides/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isProduction ? "micro-graphql-react/slides" : "",
   plugins: [react()]
-})
+});
